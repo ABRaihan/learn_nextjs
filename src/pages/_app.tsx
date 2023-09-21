ca78f00b49601c3fde348c359c8ba0febb47c661
ABRaihan <abraihan7244@gmail.com>
2023-09-21 21:34:41 +0600

    learn control custom app and how disable automatic statick optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,41 @@
-import { Layout } from "@/shared/ui";
-import "@/styles/globals.css";
-import { NextPage } from "next";
-import type { AppProps } from "next/app";
-import { ReactElement, ReactNode } from "react";
+// import { Layout } from "@/shared/ui";
+// import "@/styles/globals.css";
+// import { NextPage } from "next";
+// import type { AppProps } from "next/app";
+// import { ReactElement, ReactNode } from "react";
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
+// export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+//   getLayout?: (page: ReactElement) => ReactNode;
+// };
 
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
+// type AppPropsWithLayout = AppProps & {
+//   Component: NextPageWithLayout;
+// };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
-  return getLayout(
-    <Layout>
+// export default function App({ Component, pageProps }: AppPropsWithLayout) {
+//   const getLayout = Component.getLayout ?? ((page) => page);
+//   return getLayout(
+//     <Layout>
+//       <Component {...pageProps} />
+//     </Layout>
+//   );
+// }
+
+import App, { AppContext, AppInitialProps, AppProps } from "next/app";
+
+type AppOwnProps = { example: string };
+
+export default function MyApp({ Component, pageProps, example }: AppProps & AppOwnProps) {
+  return (
+    <>
+      <p>Data: {example}</p>
       <Component {...pageProps} />
-    </Layout>
+    </>
   );
 }
+
+MyApp.getInitialProps = async (context: AppContext): Promise<AppOwnProps & AppInitialProps> => {
+  const ctx = await App.getInitialProps(context);
+
+  return { ...ctx, example: "data" };
+};
