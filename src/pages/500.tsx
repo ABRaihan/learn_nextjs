6051e1eee14a08d63517564f1982883808026663
ABRaihan <abraihan7244@gmail.com>
2023-09-22 11:47:50 +0600

    learn how to handle error and customize error page

---
+++ src/pages/500.tsx
@@ -0,0 +1,8 @@
+const Custom500 = () => {
+  return (
+    <>
+      <h1>500 | Server Error Occur</h1>
+    </>
+  );
+};
+export default Custom500;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { Layout } from "@/shared/ui";
 import "@/styles/globals.css";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
@@ -15,8 +14,8 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   return getLayout(
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    // <Layout>
+    <Component {...pageProps} />
+    // </Layout>
   );
 }
